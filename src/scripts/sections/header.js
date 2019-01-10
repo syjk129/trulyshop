62eb19be9fdb51c084ec6b0efc8bb565c0053262
Shawn Kim <syjk129@gmail.com>
2019-01-10 11:14:18 -0500

    updates

--- src/scripts/sections/header.js
+++ src/scripts/sections/header.js
@@ -2,3 +2,32 @@ const header = document.getElementById("header-container");
 const placeholder = document.getElementById("header-placeholder");
 
 placeholder.style.height = `${header.getBoundingClientRect().height}px`;
+
+// Sidebar
+const sidebarOverlay = document.getElementById("sidebar-overlay");
+
+const cartButton = document.getElementById("header-cart");
+cartButton.onclick = () => {
+  toggleSidebar("sidebar-cart");
+}
+
+function toggleSidebar(sidebarId) {
+  const sidebar = document.getElementById(sidebarId);
+  if (sidebar.getBoundingClientRect().width === 0) {
+    sidebarOverlay.style.display = "block";
+    sidebar.style.width = "250px";
+    sidebarOverlay.addEventListener("click", sidebarClickOutside(sidebar));
+  } else {
+    sidebarOverlay.style.display = "none";
+    sidebar.style.width = "0px";
+    sidebarOverlay.removeEventListener("click", sidebarClickOutside(sidebar));
+  }
+}
+
+function sidebarClickOutside(sidebar) {
+  return function(evt) {
+    sidebar.style.width = "0px";
+    evt.preventDefault();
+    sidebarOverlay.style.display = "none";
+  }
+}
